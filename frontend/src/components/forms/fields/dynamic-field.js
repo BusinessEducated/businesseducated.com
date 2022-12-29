28de7b1002a6e06a6076d02f24539e1f6d3634b5
Aiden Faulconer <aidenf09@yahoo.com>
2022-12-29 20:19:37 +1100

    +file input (WIP) +fixed some pages +changed design on index page

--- frontend/src/components/forms/fields/dynamic-field.js
+++ frontend/src/components/forms/fields/dynamic-field.js
@@ -27,7 +27,6 @@ export const DynamicField = ({
 }) => {
     const { name, type, component, icon, placeholder, required, options, max, allow, cols, validationType } = fieldSchema;
 
-    
     //validation and input tracking in store.js 
     const components = {
         select: Select,
@@ -46,15 +45,12 @@ export const DynamicField = ({
     }
     const InputComponent = components[type] || (({name})=>{return(<p>broken schema for {name}'s {type} component</p>)});
     
-    // onChange={setFormInput} value={input}
     return (
         <div className={`sm:col-span-${cols} col-span-6 relative`} key={`field-${name} form-field`}>
-            {/* {JSON.stringify(InputComponent,null,2)} */}
-                <InputComponent error={errors.length > 0} onChange={onChange} value={value} {...fieldSchema}  />
+            <InputComponent error={errors.length > 0} onChange={onChange} value={value} {...fieldSchema}  />
             {errors.length > 0 && (
                 <span className="rounded-md text-red-500 flex gap-3 absolute right-0 -top-[.15rem] items-center">
-                    {/* <ExclamationCircleIcon className="w-4 h-4 fill-BEred" /> */}
-                    {/* {errors.map(err=>(err))} */}
+                    {/* <ExclamationCircleIcon className="w-4 h-4 fill-BEred" /> */} 
                     {errors}
                 </span>
             )}
