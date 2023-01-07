1fdbfff39bd28cced8f12eb1199053da14f88c84
Aiden Faulconer <aidenf09@yahoo.com>
2023-01-07 21:45:20 +1100

    +setup server for deployment **WIP nginx and pm2 configuration**

---
+++ backend/server/index.js
@@ -0,0 +1,27 @@
+const serverless = require('serverless-http')
+const { app, router } = require('./src/index')
+
+require('dotenv').config({
+  path: `${__dirname}/../../.env.${process.env.NODE_ENV}`,
+})
+
+// ========================================================================== //
+// Main server *redirects to https*
+// ========================================================================== //
+app.listen(process.env.SERVER_PORT, () =>
+  console.log(`
+    HTTP server listening: http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}!\n
+    SWAGGER: http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}/api-docs!\n
+`),
+)
+
+// ========================================================================== //
+// HTTPS
+// ========================================================================== //
+// HTTPS server
+// const httpsServer = https.createServer({
+//   key: fs.readFileSync(path.join(process.cwd(), 'netlify/functions/src/', 'server.key')),
+//   cert: fs.readFileSync(path.join(process.cwd(), 'netlify/functions/src/', 'server.cert')),
+// }, app);
+// app.all('*', (req, res) => res.redirect(`https://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}`, 200));
+// httpsServer.listen(443, () => console.log('HTTPS server listening: https://localhost:443'));
