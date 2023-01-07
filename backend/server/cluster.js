df760d29300716570143deec6bc1f5b8d4fe25e5
Aiden Faulconer <aidenf09@yahoo.com>
2023-01-07 21:45:24 +1100

    +setup server for deployment **WIP nginx and pm2 configuration**

--- backend/server/cluster.js
+++ backend/server/cluster.js
@@ -1,24 +1,65 @@
-const serverless = require('serverless-http');
-const { app, router } = require('./src/index');
+const cluster = require('cluster')
+const http = require('http')
+const numCPUs = require('os').cpus().length
 
-require('dotenv').config({ path: `${__dirname}/../../.env.${process.env.NODE_ENV}`});
+const serverless = require('serverless-http')
+const { app, router } = require('./src/index')
+
+require('dotenv').config({
+  path: `${__dirname}/../../.env.${process.env.NODE_ENV}`,
+})
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  app.listen(process.env.SERVER_PORT, () =>
+    console.log(`
+    HTTP server listening: http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}!\n
+    SWAGGER: http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}/api-docs!\n
+`),
+  )
+
+  console.log(`Worker ${process.pid} started`)
+}
+
+const serverless = require('serverless-http')
+const { app, router } = require('./src/index')
+
+require('dotenv').config({
+  path: `${__dirname}/../../.env.${process.env.NODE_ENV}`,
+})
 
 // ========================================================================== //
 // add our api into the express app for serverless to use
-app.use('/.backend/server', router);// route to the main lambda function netlify uses
+app.use('/.backend/server', router) // route to the main lambda function netlify uses
 
 // ========================================================================== //
 // The listender for the lambda function
-module.exports.handler = serverless(app, { debug: process.env.NODE_ENV === 'development' });
+module.exports.handler = serverless(app, {
+  debug: process.env.NODE_ENV === 'development',
+})
 
 // ========================================================================== //
 // Main server *redirects to https*
 // ========================================================================== //
 // app.all('*', (req, res) => res.redirect(`http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}`, 200));
-app.listen(process.env.SERVER_PORT, () => console.log(`
+app.listen(process.env.SERVER_PORT, () =>
+  console.log(`
     HTTP server listening: http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}!\n
     SWAGGER: http://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}/api-docs!\n
-`));
+`),
+)
 
 // ========================================================================== //
 // HTTPS
@@ -31,5 +72,3 @@ app.listen(process.env.SERVER_PORT, () => console.log(`
 // }, app);
 // app.all('*', (req, res) => res.redirect(`https://${process.env.DOMAIN_NAME}:${process.env.SERVER_PORT}`, 200));
 // httpsServer.listen(443, () => console.log('HTTPS server listening: https://localhost:443'));
- 
- 
